07b27e3b9ac414c99a5865722d6905d9bda91940
KacperTetela1 <kacper.tetela@student.wsb.edu.pl>
2023-11-30 16:50:36 +0100

    Initial Commit

---
+++ src/main/java/com/csmanager/MainGUI.java
@@ -0,0 +1,7 @@
+package com.csmanager;
+
+public class MainGUI {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/com/csmanager/CsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
