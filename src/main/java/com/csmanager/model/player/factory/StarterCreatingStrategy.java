b0853221364d815b46a5dbd7ecaa47a76db7baa6
KacperTetela <kacpertetela1@gmail.com>
2024-04-13 16:25:47 +0200

    StrategyPlayerFactor

--- src/main/java/com/csmanager/model/player/factory/StarterCreatingStrategy.java
+++ src/main/java/com/csmanager/model/player/factory/StarterCreatingStrategy.java
@@ -1,10 +1,19 @@
 package com.csmanager.model.player.factory;
 
 import com.csmanager.model.player.builder.Player;
+import com.github.javafaker.Faker;
 
 public class StarterCreatingStrategy implements PlayerCreatingStrategy{
+    private String[] names;
+    public StarterCreatingStrategy() {
+        Faker faker = new Faker();
+    }
+
     @Override
     public Player createPlayer() {
         return null;
     }
+
+
+
 }
