49ff28f6450739d217aa76b725d559233916f89a
kacpertetela <kacpertetela1@gmail.com>
2024-05-05 22:53:41 +0200

    Adding NicknamesFaker Class

--- src/main/java/com/csmanager/model/player/player/creatingStrategy/AboveCreatingStrategy.java
+++ src/main/java/com/csmanager/model/player/player/creatingStrategy/AboveCreatingStrategy.java
@@ -3,14 +3,12 @@ package com.csmanager.model.player.player.creatingStrategy;
 import com.csmanager.model.player.player.Player;
 import com.csmanager.model.player.player.PlayerBuilder;
 import com.csmanager.model.player.performance.Difficulty;
-import com.github.javafaker.Faker;
+import com.csmanager.utils.NicknamesFaker;
 
 public class AboveCreatingStrategy implements PlayerCreatingStrategy {
-    private final Faker faker = new Faker();
-
     @Override
     public Player createPlayer() {
-        PlayerBuilder playerBuilder = new PlayerBuilder().name(faker.name().name())
+        PlayerBuilder playerBuilder = new PlayerBuilder().name(NicknamesFaker.getNickname())
                 .difficulty((Difficulty.getInstance().getValue()) + 0.2);
         return playerBuilder.build();
     }
--- src/main/java/com/csmanager/model/player/player/creatingStrategy/DefaultCreatingStrategy.java
+++ src/main/java/com/csmanager/model/player/player/creatingStrategy/DefaultCreatingStrategy.java
@@ -3,14 +3,12 @@ package com.csmanager.model.player.player.creatingStrategy;
 import com.csmanager.model.player.player.Player;
 import com.csmanager.model.player.player.PlayerBuilder;
 import com.csmanager.model.player.performance.Difficulty;
-import com.github.javafaker.Faker;
+import com.csmanager.utils.NicknamesFaker;
 
 public class DefaultCreatingStrategy implements PlayerCreatingStrategy {
-    private final Faker faker = new Faker();
-
     @Override
     public Player createPlayer() {
-        PlayerBuilder playerBuilder = new PlayerBuilder().name(faker.name().username())
+        PlayerBuilder playerBuilder = new PlayerBuilder().name(NicknamesFaker.getNickname())
                 .difficulty(Difficulty.getInstance().getValue());
         return playerBuilder.build();
     }
